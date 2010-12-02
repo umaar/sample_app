771b5aeed23e39539b771da2cac609385698322f
umaar <admin@umaar.com>
2010-12-02 22:55:16 +0000

    finished static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,14 @@
 class PagesController < ApplicationController
   def home
+  	@title = "Home"
   end
 
   def contact
+  	@title = "Contact"
+  end
+  
+  def about
+  	@contact = "About"
   end
 
 end
